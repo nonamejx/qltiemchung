b3756a92809b37aa230ffc5d8f0b949de9a98eba
nonamejx <huynhbahai94@gmail.com>
2016-04-24 15:49:43 +0700

    Them UrlUtils, UrlCollection

---
+++ qltiemchung/src/com/qltiemchung/utils/UrlUtils.java
@@ -0,0 +1,32 @@
+package com.qltiemchung.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.qltiemchung.model.bean.UrlCollection;
+
+public class UrlUtils {
+
+	private HttpServletRequest request;
+
+	private static UrlUtils instance;
+
+	public static UrlUtils getInstance(HttpServletRequest request) {
+		if (instance == null) {
+			return new UrlUtils(request);
+		}
+		return instance;
+	}
+
+	public UrlUtils(HttpServletRequest request) {
+		this.request = request;
+	}
+
+	public String getPath(UrlCollection urlCollection) {
+		if (request == null) {
+			return null;
+		} else {
+			return this.request.getContextPath().concat(urlCollection.getUrl());
+		}
+
+	}
+}
